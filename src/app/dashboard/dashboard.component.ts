c406c84fcbda0f5f0f94419f48e970edb6dce8df
Terrance <terrance.hanlon@ndsu.edu>
2018-09-10 19:23:42 -0500

    show all breeds

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,9 +10,17 @@ export class DashboardComponent implements OnInit {
 
   constructor(private endpointInfo: EndpointInfoService) { }
 
+  breedList = [];
+
   ngOnInit() {
     this.endpointInfo.listAllBreeds().subscribe((response) => {
       console.log(response);
+      for(let breed in response['message'])
+      {
+        this.breedList.push(breed);
+      }
+      console.log(this.breedList);
+
     })
   }
 
