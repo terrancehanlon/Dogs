2e17738920e0bbd2cefc23b9de5a450e7957123a
Terrance <terrance.hanlon@ndsu.edu>
2018-09-12 21:15:39 -0500

    add server.js

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+var router = express.Router();
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Dogs'));
+app.use(express.static('./dist/dogs'));
 
-app.get('/*', function(req,res) {
+// app.use('/*', express.static(path.resolve('dist/dogs/index.html')));
+// app.use('./dist/main', express.static(__dirname + '/dist/dogs/main.7bfa94303bec8c0ed597.js'));
+// app.use('./dist/polyfills', express.static(__dirname + '/dost/dogs/polyfills.2f4a59095805af02bd79.js'));
+// app.use('*', express.static(__dirname+ '/dist/dogs/runtime.a66f828dca56eeb90e02.js'));
+// app.use('./dist/styles', express.static(__dirname+ '/dist/dogs/styles.34c57ab7888ec1573f9c.css'));
+// router.get('*', express.static(path.resolve('dist/dogs/index.html')));
+app.get('*', function(req, res) {
     console.log(__dirname);
-res.sendFile(path.join(__dirname+'/src/index.html'));
-});
+    res.sendFile(__dirname + '/dist/dogs/index.html');
+})
+
+
+
+// app.get('/*', function(req,res) {
+//     console.log(__dirname);
+//     ///home/terrance/Desktop/projs/Angular/Dogs/dogs/src/index.html
+// res.sendFile(path.join(__dirname+'/dist/dogs/index.html'));
+// ///home/terrance/Desktop/projs/Angular/Dogs/dogs/dist/dogs/index.html
+// });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+// /home/terrance/Desktop/projs/Angular/Dogs/dogs/dist/dogs/index.html
